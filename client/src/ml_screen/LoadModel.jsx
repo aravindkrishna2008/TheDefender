9ff560037c10e5db11dc60e34b40ecfb33147a5f
The-Coding-Kid <veerrohitv08@gmail.com>
2021-11-27 15:51:01 -0800

    moving everything to LoadModel.jsx, CameraScreen.jsx is gone now

--- client/src/ml_screen/LoadModel.jsx
+++ client/src/ml_screen/LoadModel.jsx
@@ -1,5 +1,64 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, Image, Button, Input } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import * as tf from "@tensorflow/tfjs";
 import { fetch, bundleResourceIO } from "@tensorflow/tfjs-react-native";
 import * as blazeface from "@tensorflow-models/blazeface";
+import RNPickerSelect from "react-native-picker-select";
+import { Button } from "react-native-paper";
+import { Camera } from "expo-camera";
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <Button
+            style={styles.button}
+            mode="contained"
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={styles.text}> Flip </Text>
+          </Button>
+        </View>
+      </Camera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  camera: {
+    height: 320,
+    width: 180,
+    marginBottom: 300,
+  },
+});

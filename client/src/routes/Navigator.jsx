06908d76eb73e2deb79c8f747bbea82a18233e67
The-Coding-Kid <veerrohitv08@gmail.com>
2021-11-28 07:55:41 -0800

    formatted all the files

--- client/src/routes/Navigator.jsx
+++ client/src/routes/Navigator.jsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import CameraScreen from '../screens/CameraScreen'
-import SignInScreen from '../screens/SignInScreen'
-import { navigatorRef } from '../RootNavigation'
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CameraScreen from "../screens/CameraScreen";
+import SignInScreen from "../screens/SignInScreen";
+import { navigatorRef } from "../RootNavigation";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export function MyStack() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={SignInScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={SignInScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

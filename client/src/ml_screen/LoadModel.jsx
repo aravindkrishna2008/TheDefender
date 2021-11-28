b776e6cc94e014428c382198180438b4601315e6
The-Coding-Kid <veerrohitv08@gmail.com>
2021-11-27 20:21:16 -0800

    want aravind to work on a camera button for me for now we will fix it later

--- client/src/ml_screen/LoadModel.jsx
+++ client/src/ml_screen/LoadModel.jsx
@@ -16,151 +16,20 @@ import { Camera } from "expo-camera";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 import { cameraWithTensors } from "@tensorflow/tfjs-react-native";
 import { LogBox } from "react-native";
+import * as FaceDetector from 'expo-face-detector';
 //disable yellow warnings on EXPO client!
 LogBox.ignoreAllLogs = true;
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [blazeFaceModel, setBlazeFaceModel] = useState(null);
-  const [textureDimsState, setTextureDims] = useState();
-  const [blazeFacePrediction, setBlazeFacePrediction] = useState();
-  const [type, setType] = useState(Camera.Constants.Type.front);
-  const [modelFaces, setModelFaces] = useState([]);
-  const [isTFReady, setTFReady] = useState(false);
-  const [loadedModel, setModelLoaded] = useState(null);
-  const [modelPrediction, setModelPrediction] = useState();
-  const [frameWorkReady, setFrameWorkReady] = useState(false);
-  const [finalPrediction, setFinalPrediction] = useState();
-  const [predictionReady, setPredictionReady] = useState(false);
-  const [allPredictions, setAllPredictions] = useState({
-    0: [],
-    5: [],
-    10: [],
-  });
-
-  let requestAnimationFrameId = 0;
-  let frameCount = 0;
-  let makePredictionsEveryNFrames = 1;
-  let queueSize = 0;
-  const AUTORENDER = true;
-
-  // const modelJSON = require("../../assets/machine_learning/model.json");
-  // const modelWeights = require("../../assets/machine_learning/group1-shard1of1.bin");
-
-  const tensorDims = { height: 200, width: 200, depth: 3 };
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
-    if (!frameWorkReady) {
-      (async () => {
-        const { status } = await Camera.requestPermissionsAsync().catch((e) =>
-          console.log(e)
-        );
-        if (Platform.OS == "ios") {
-          setTextureDims({ height: 1920, width: 1080 });
-        } else {
-          setTextureDims({ height: 1200, width: 1600 });
-        }
-        setHasPermission(status === "granted");
-        await tf.ready().catch((e) => console.log(e));
-        setTFReady(true);
-        setModelLoaded(await loadModel().catch((e) => console.log(e)));
-        setBlazeFaceModel(
-          await loadBlazeFaceModel().catch((e) => console.log(e))
-        );
-        setFrameWorkReady(true);
-      })();
-    }
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
   }, []);
-
-  useEffect(() => {
-    return () => {
-      cancelAnimationFrame(requestAnimationFrameId);
-    };
-  }, [requestAnimationFrameId]);
-
-  const getPrediction = async (tensor) => {
-    if (!tensor) {
-      console.log("Tensor not found!");
-    }
-    const bfModel = await blazeFaceModel;
-    const returnTensors = true;
-    const faces = await bfModel
-      .estimateFaces(tensor, returnTensors)
-      .catch((e) => console.log(e));
-    const scale = {
-      height: styles.camera.height / tensorDims.height,
-      width: styles.camera.width / tensorDims.width,
-    };
-
-    if (!isEmpty(faces)) {
-      setModelFaces({ faces });
-      faces.map((face, i) => {
-        const { topLeft, bottomRight } = face;
-        // Boxes in cropAndResize require to be normalized
-        const normTopLeft = topLeft.div(tensor.shape.slice(-3, -2));
-        const normBottomRight = bottomRight.div(tensor.shape.slice(-3, -2));
-        const width = Math.floor(
-          bottomRight.dataSync()[0] - topLeft.dataSync()[0] * scale.width
-        );
-        const height = Math.floor(
-          bottomRight.dataSync()[1] - topLeft.dataSync()[1] * scale.height
-        );
-        const boxes = tf
-          .concat([normTopLeft.dataSync(), normBottomRight.dataSync()])
-          .reshape([-1, 4]);
-        const crop = tf.image.cropAndResize(
-          tensor.reshape([1, 200, 200, 3]),
-          boxes,
-          [0],
-          [height, width]
-        );
-        // Resize cropped faces to [1,224,224,3]
-        const alignCorners = true;
-        const imageResize = tf.image.resizeBilinear(
-          crop,
-          [200, 200],
-          alignCorners
-        );
-        makePrediction(imageResize);
-      });
-    }
-  };
-
-  const makePrediction = async (image) => {
-    if (!image) {
-      console.log("No input!");
-    }
-    const model = await loadedModel;
-    const prediction = await model.predict(image, { batchSize: 1 });
-    if (!prediction || isEmpty(prediction)) {
-      console.log("Prediction not available");
-    }
-    rollingPrediction(prediction.dataSync());
-  };
-
-  // Handling the camera input and converting it into tensors to be used in the
-  // model for predictions
-  const handleCameraStream = (imageAsTensors) => {
-    if (!imageAsTensors) {
-      console.log("Image not found!");
-    }
-    const loop = async () => {
-      if (frameCount % makePredictionsEveryNFrames === 0) {
-        const imageTensor = imageAsTensors.next().value;
-
-        if (loadedModel !== null && blazeFaceModel !== null) {
-          await getPrediction(imageTensor).catch((e) => console.log(e));
-        }
-        tf.dispose(imageAsTensors);
-      }
-
-      frameCount += 1;
-      frameCount = frameCount % makePredictionsEveryNFrames;
-      requestAnimationFrameId = requestAnimationFrame(loop);
-    };
-    //loop infinitely to constantly make predictions
-    loop();
-  };
 
   if (hasPermission === null) {
     return <View />;
@@ -173,7 +42,7 @@ export default function App() {
       <Camera style={styles.camera} type={type}>
         <View style={styles.buttonContainer}>
           <Button
-            style={styles.button}
+            style={{marginTop: 100}}
             mode="contained"
             onPress={() => {
               setType(
